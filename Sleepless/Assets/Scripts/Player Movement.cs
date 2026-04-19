3373e5c892f5794248e2081c32af007a63fe2a80
Hajin1221 <130419285+Evrin1221@users.noreply.github.com>
2026-04-18 17:07:40 -0700

    back and forth done

--- Sleepless/Assets/Scripts/Player Movement.cs
+++ Sleepless/Assets/Scripts/Player Movement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float _minSpeed;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
     // Start is called before the first frame update
 
     void Start()
@@ -17,5 +19,23 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         transform.position = transform.position + new Vector3(1, 0, 0) * _minSpeed * Time.deltaTime;
+
+        Movement();
+    }
+
+
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + new Vector3(1, 0, 0) * _speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + new Vector3(-1, 0, 0) * _speed * Time.deltaTime;
+        }
+
+
     }
 }
