f87f182cecdbe8381c227a61459a030f2cb1d854
Hajin1221 <130419285+Evrin1221@users.noreply.github.com>
2026-04-22 17:10:04 -0700

    dshfsjh

--- Sleepless/Assets/Scripts/Player Movement.cs
+++ Sleepless/Assets/Scripts/Player Movement.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    //basic movement
     [SerializeField] private float _minSpeed;
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
-    private float _backSpeed;
-    // Start is called before the first frame update
+    [SerializeField] private float _backSpeed;
+    [SerializeField] private Rigidbody2D _rb;
+
+    private bool _isGrounded;
+
+
+    //fancy jump shit
+
+    //variable jump height
+
+    [SerializeField]private float _jumpCutMultiplier;
+    [SerializeField]private float _maxJumpHoldTime;
+    private bool _isJumping;
+    private bool _jumpHeld;
+    private float _jumpTimer = 0f;
 
     void Start()
     {
-        _backSpeed = _minSpeed + _speed;
+        _rb = Locator.Instance._player.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,6 +35,7 @@ public class PlayerMovement : MonoBehaviour
         transform.position = transform.position + new Vector3(1, 0, 0) * _minSpeed * Time.deltaTime;
 
         Movement();
+        Jump();
     }
 
 
@@ -38,5 +52,44 @@ public class PlayerMovement : MonoBehaviour
         }
 
 
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+            _isGrounded = false;
+            _jumpTimer = 0f;
+            _isJumping = true;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && _isJumping)
+        {
+            JumpTimer();
+        }
+
+        if ((_jumpTimer >= _maxJumpHoldTime || Input.GetKeyUp(KeyCode.Space)) && _isJumping)
+        {
+            _isJumping = false;
+            _jumpTimer = 0f;
+
+            if (_rb.velocity.y > 0f)
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y * _jumpCutMultiplier);
+            }
+        }
+    }
+    private  void JumpTimer()
+    {
+        _jumpTimer += Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+        }
     }
 }
