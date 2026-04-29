08f4cf0be3f8d349417da28f5857d7b67bcfa7df
Hajin1221 <130419285+Evrin1221@users.noreply.github.com>
2026-04-28 18:48:12 -0700

    platforms added

--- Sleepless/Assets/Scripts/AutoScroller.cs
+++ Sleepless/Assets/Scripts/AutoScroller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ground : MonoBehaviour
+public class AutoScroller : MonoBehaviour
 {
 
     [SerializeField] private float _minSpeed;
